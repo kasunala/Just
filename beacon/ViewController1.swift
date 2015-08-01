978612e1a7803269ad30030df4f022c36d0e0516
kasunala <kasunala@gmail.com>
2015-08-01 08:19:58 +0530

    Deploying cloud code
    Ad-Hoc Backup v2
    
    function : BackupMessages

---
+++ beacon/ViewController1.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController1.swift
+//  beacon
+//
+//  Created by Kasun Alahakoon on 7/29/15.
+//  Copyright (c) 2015 holosofts. All rights reserved.
+//
+import UIKit
+import Foundation
+
+
+class ViewConroller1 {
+
+    
+    
+    
+}
---
+++ cloud/beacon/cloud/main.js
@@ -0,0 +1,66 @@
+
+//Kasun Alahakoon on 1st August 2015
+
+Parse.Cloud.job("BackupMessages", function(request, status) {
+
+    var nowTimeStamp = Date.now();
+
+    var timeout = 60 //* 60 * 1000; // one hour //test 1 min
+
+    var query = new Parse.Query("Calls");
+
+    // set a limit to make sure resource usage
+
+    query.limit(100); 
+
+    query.equalTo("read", true);
+
+    query.lessThan("readTime", nowTimeStamp - timeout);
+
+    query.find().then(function(messages) {
+
+        var promises = [];
+
+        var CallsArchive = Parse.Object.extend("CallsArchive");
+
+        _.each(messages, function(message) {
+
+            promises.push(function(message) {
+
+                var newObject = new CallsArchive();
+
+                // try to backup the ObjectId of Calls
+
+                newObject.set("CallsId", message.get("objectId"));
+
+                // for each data column you want to backup
+
+                newObject.set("data", message.get("data"));
+
+                // ...
+
+                return newObject.save();
+
+            } (message));
+
+        });
+
+        // remember clean "Calls"
+
+        // promises.push(Parse.Object.destroyAll(messages));
+
+        // do Promises in Parallel 
+
+        return Parse.Promise.when(promises);
+
+    }).then(function() {
+
+        status.success("data moved.");
+
+    }, function(error) {
+
+        status.error("error move data" + error.code + ": " + error.message);
+
+    });
+
+});
